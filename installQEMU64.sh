60e43b1218f230176c22b372dd3745514193fcf1
AkiraYuki <44525248+AllPlatform@users.noreply.github.com>
2020-11-22 21:38:00 +0700

    Delete installQEMU64.sh

--- installQEMU64.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-chmod 777 start-raspi64.sh;
-wget http://ftp.br.debian.org/debian/pool/main/q/qemu/qemu-user-static_3.1+dfsg-8+deb10u7_arm64.deb;
-dpkg --extract ./qemu-user-static_3.1+dfsg-8+deb10u7_arm64.deb ./;
-chmod 777 ./usr/bin/*;
-cp -f ./usr/bin/* ~/../usr/bin;
-rm -fR ./qemu-*.deb ./usr ;
