5d4ae68a3c8cea9d64cd9751d26c482a54865969
AkiraYuki <44525248+AllPlatform@users.noreply.github.com>
2020-11-22 21:37:09 +0700

    Delete install-FULL.sh

--- install-FULL.sh
+++
@@ -1,1 +0,0 @@
-chmod +x start-raspi.sh && pkg install wget -y && pkg install proot -y && mkdir raspi && cd raspi && wget http://downloads.raspberrypi.org/raspbian_full/archive/2019-04-09-22:49/root.tar.xz && proot --link2symlink tar -xJf root.tar.xz && rm root.tar.xz && cd --
